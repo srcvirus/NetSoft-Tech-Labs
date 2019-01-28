98bec8f5c637ea6ec413bc18f7627597acb88c5e
Shihab <shihab.buet@gmail.com>
2019-01-28 10:35:26 -0500

    Cleanup obsolete files.

--- nv-lab/map-vport-to-pport.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export OVX_HOME="~/OpenVirteX"
-python $OVX_HOME/utils/ovxctl.py -n createPort 1 00:00:00:00:00:00:00:01 1
-python $OVX_HOME/utils/ovxctl.py -n createPort 1 00:00:00:00:00:00:00:01 2
-python $OVX_HOME/utils/ovxctl.py -n createPort 1 00:00:00:00:00:00:00:03 1
-python $OVX_HOME/utils/ovxctl.py -n createPort 1 00:00:00:00:00:00:00:03 3
-python $OVX_HOME/utils/ovxctl.py -n createPort 1 00:00:00:00:00:00:00:06 1
-python $OVX_HOME/utils/ovxctl.py -n createPort 1 00:00:00:00:00:00:00:06 3
--- nv-lab/map-vswitch-to-pswitch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export OVX_HOME="~/OpenVirteX"
-python $OVX_HOME/utils/ovxctl.py -n createSwitch 1 00:00:00:00:00:00:00:01
-python $OVX_HOME/utils/ovxctl.py -n createSwitch 1 00:00:00:00:00:00:00:03
-python $OVX_HOME/utils/ovxctl.py -n createSwitch 1 00:00:00:00:00:00:00:06
